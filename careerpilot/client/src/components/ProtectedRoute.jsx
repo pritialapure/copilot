2896bff25fca744659b5a629b981bc2e753738ba
Priti <pritialapure@gmail.com>
2026-09-03 01:05:33 +0530

    .

--- careerpilot/client/src/components/ProtectedRoute.jsx
+++ careerpilot/client/src/components/ProtectedRoute.jsx
@@ -2,9 +2,9 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore';
 
 export default function ProtectedRoute() {
-  const { user, token } = useAuthStore();
+  const { user } = useAuthStore();
 
-  if (!user || !token) {
+  if (!user) {
     return <Navigate to="/login" replace />;
   }
 
